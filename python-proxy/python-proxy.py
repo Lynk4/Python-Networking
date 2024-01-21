f338151fd1d8a81408e2fae37b634b1dcfa87252
Chandra Kant <kant@APPLEs-MacBook-Pro.local>
2024-01-21 07:48:16 +0530

    python-proxy

---
+++ python-proxy/python-proxy.py
@@ -0,0 +1,9 @@
+import requests
+
+proxies = {
+    'https': 'https://43.134.91.82:3128'
+}
+response = requests.get("https://ipinfo.io/json", proxies=proxies)
+
+print(response.json()['country'])
+print(response.text)
